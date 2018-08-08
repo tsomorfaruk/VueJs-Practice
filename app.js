9fd495ff3f559a02cc23d98a18dcb9e3dd769ec1
tsomorfaruk <tsomorfaruk@gmail.com>
2018-08-08 12:21:41 +0600

    Data, Method and Event complete

--- app.js
+++ app.js
@@ -1,11 +1,33 @@
+/* Data Binding
 new Vue({
     el:"#root",
     data: {
-        name: "omor"
+        name: "omor",
+        job: 'student',
+        website: 'https://www.google.com',
+        websiteTag: '<a href=https://www.google.com>Omor faruk</a>'
+    }
+});*/
+//Events
+
+new Vue({
+    el: '#root',
+    data: {
+        age: '25',
+        x: '0',
+        y: '0'
     },
-    method: {
-        greed: function(time){
-            return "good "+ time;
+    methods: {
+        add: function(inc) {
+            this.age += inc;
+        },
+        minus: function(dec) {
+            this.age -= dec;
+        },
+        updateXY: function (event) {
+            this.x = event.offsetX;
+            this.y = event.offsetY;
         }
     }
 });
+
