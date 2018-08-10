1c1ec4debf7df0cfbfd8179f8ff0a87d4a9688be
tsomorfaruk <tsomorfaruk@gmail.com>
2018-08-10 16:02:54 +0600

    Computed Properties complete

--- app.js
+++ app.js
@@ -8,12 +8,12 @@ new Vue({
         websiteTag: '<a href=https://www.google.com>Omor faruk</a>'
     }
 });*/
-//Events
+/*Mouse Events
 
 new Vue({
     el: '#root',
     data: {
-        age: '25',
+        age: 25,
         x: '0',
         y: '0'
     },
@@ -27,7 +27,62 @@ new Vue({
         updateXY: function (event) {
             this.x = event.offsetX;
             this.y = event.offsetY;
+        },
+        click: function () {
+            alert("are u click???");
+        }
+    }
+});*/
+
+/*Keyboard Events
+new Vue({
+    el:'#root',
+    data: {
+
+    },
+    methods: {
+        logName: function () {
+            console.log("Entered Your Name");
+        },
+        logAge: function () {
+            console.log("Entered Your Age");
+        },
+    }
+});
+//Two way Data Binding
+new Vue({
+    el: '#root',
+    data: {
+        name: '',
+        age: '',
+    },
+});*/
+// Computed Properties
+new Vue({
+    el: '#root',
+    data: {
+        age: 20,
+        a: 0,
+        b:0
+    },
+    methods: {
+        /*addToA: function () {
+            console.log("Your age A");
+            return this.a + this.age;
+        },
+        addToB: function () {
+            console.log("your age b");
+            return this.b + this.age;
+        },*/
+    },
+    computed: {
+        addToA: function () {
+            console.log("Your age A");
+            return this.a + this.age;
+        },
+        addToB: function () {
+            console.log("your age b");
+            return this.b + this.age;
         }
     }
 });
-
